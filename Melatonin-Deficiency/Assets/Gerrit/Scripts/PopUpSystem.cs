8781225e4a4b601cc93b422e057bd236bcc23343
Gerritbrink <97100026@st.deltion.nl>
2023-06-13 11:34:52 +0200

    Animation, pop up, some Prep for math.f

--- Melatonin-Deficiency/Assets/Gerrit/Scripts/PopUpSystem.cs
+++ Melatonin-Deficiency/Assets/Gerrit/Scripts/PopUpSystem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PopUpSystem : MonoBehaviour
@@ -8,12 +9,46 @@ public class PopUpSystem : MonoBehaviour
 
     public GameObject popUpBox;
     public Animator animator;
-    public TMP_Text popUpText;
-
-    public void PopUp(string text)
+    private void Start()
     {
-        popUpBox.SetActive(true);
-        popUpText.text = text;
-        animator.SetTrigger("pop");
+            popUpBox.SetActive(false);
+            Cursor.visible = false;
     }
+    
+    public void Update() 
+    { 
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+           
+            if (!popUpBox.activeSelf)
+            {
+                popUpBox.SetActive(true);
+                animator.SetTrigger("pop");
+            }
+            else
+            {
+                animator.SetTrigger("close");
+                StartCoroutine(Close());
+            }
+
+            if (!popUpBox.activeSelf)
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+        }
+    }
+
+    private IEnumerator Close ()
+    {
+        yield return new WaitForSeconds(1f);
+        popUpBox.SetActive(false);
+
+    }
+
 }
---
+++ Melatonin-Deficiency/Assets/Gerrit/Scripts/UISysteem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UISysteem : MonoBehaviour
+{
+    public Slider healthP;
+    public Slider stamp;
+    public ItemsProperties props;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
