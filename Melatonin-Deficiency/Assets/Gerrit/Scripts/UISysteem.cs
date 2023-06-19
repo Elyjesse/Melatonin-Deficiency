113fe30ab146312e7f4dc5b07cf80ef077abf234
Gerritbrink <97100026@st.deltion.nl>
2023-06-19 15:08:34 +0200

    Stamina systeem aan het ui conecten

--- Melatonin-Deficiency/Assets/Gerrit/Scripts/UISysteem.cs
+++ Melatonin-Deficiency/Assets/Gerrit/Scripts/UISysteem.cs
@@ -1,23 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
 
 public class UISysteem : MonoBehaviour
 {
     public Slider healthP;
-    public Slider stamp;
+    public Slider stamP;
     public ItemsProperties props;
-
-    void Start()
+    public Stamina rechargingRn1;
+    public Stamina rechargingRn2;
+    public Stamina rechargeRate;
+    public Stamina currentPlayerStam;
+    public void SetStamina(int currentPlayerStam)
     {
-        
-    }
-
-
-    void Update()
-    {
-        
+        stamP.value = currentPlayerStam;
     }
 }
